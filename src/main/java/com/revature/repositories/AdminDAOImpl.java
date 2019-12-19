32bfbe2089c4ced5f274803b87b1206abdda9904
stefani <stefanimarieangel@gmail.com>
2019-12-18 21:15:50 -0500

    added admin and employeeDOAImpl

---
+++ src/main/java/com/revature/repositories/AdminDAOImpl.java
@@ -0,0 +1,39 @@
+package com.revature.repositories;
+
+import java.util.List;
+
+import com.revature.models.Admin;
+
+public class AdminDAOImpl implements AdminDAO {
+
+	@Override
+	public List<Admin> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Admin findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Admin findByUsername(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean insert(Admin a, String password) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Admin a) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/main/java/com/revature/repositories/EmployeeDAOImpl.java
@@ -0,0 +1,39 @@
+package com.revature.repositories;
+
+import java.util.List;
+
+import com.revature.models.Employee;
+
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+	@Override
+	public List<Employee> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee findByUsername(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean insert(Employee e, String password) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Employee e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
